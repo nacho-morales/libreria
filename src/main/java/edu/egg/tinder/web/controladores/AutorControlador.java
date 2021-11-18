2f898fdb5a3f7a19d12cac06fbef9780ce544b83
nacho-morales <89043382+nacho-morales@users.noreply.github.com>
2021-11-18 17:38:28 -0300

    antes de admin

--- src/main/java/edu/egg/tinder/web/controladores/AutorControlador.java
+++ src/main/java/edu/egg/tinder/web/controladores/AutorControlador.java
@@ -27,55 +27,55 @@ import org.springframework.web.multipart.MultipartFile;
  *
  * @author ignacio
  */
-@PreAuthorize("hasAnyRole('ROLE_USUARIO_REGISTRADO')")
-@Controller
-@RequestMapping
-public class AutorControlador {
-
-    @Autowired
-    private AutorServicio clienteServicios;
-
-    @GetMapping("/editar-perfilmascota")
-    public String editarPerfil(HttpSession session, @RequestParam(required=false) Integer id, ModelMap model) {
-
-        
-        return "autor.html";
-    }
-
-    /**
-     *
-     * @param archivo
-     * @param modelo
-     * @param session
-     * @param id
-     * @param nombre
-     * @param apellido
-     * @param email
-     * @param documento
-     * @param clave
-     * @param clave2
-     * @param idZona
-     * @return
-     */
-    @PreAuthorize("hasAnyRole('ROLE_USUARIO_REGISTRADO')")
-    @PostMapping("/actualizar-perfilmascota")
-    public String registrar(ModelMap modelo, HttpSession session, @RequestParam Integer id, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String email, @RequestParam String documento, MultipartFile archivo, @RequestParam String clave, @RequestParam String clave2, @RequestParam Integer idZona) {
-        Cliente cliente = null;
-        Cliente login = (Cliente) session.getAttribute("usuariosession");
-        if (login == null || !login.getId().equals(id)) {
-            return "redirect:/inicio";
-        }
-        try {
-            cliente = clienteServicios.buscarXId(id);
-            clienteServicios.modificarCliente(id, documento, nombre, apellido, email, archivo, clave, clave2, idZona);
-            session.setAttribute("usuariosession", cliente);
-            return "redirect:/inicio";
-        } catch (ErrorServicio ex) {
-            List<Zona> zonas = zonaRepositorio.findAll();
-            modelo.put("zonas", zonas);
-            modelo.put("error", ex.getMessage());
-            modelo.put("perfil", cliente);
-            return "perfil.html";
-        }
-    }
-}
+//@PreAuthorize("hasAnyRole('ROLE_USUARIO_REGISTRADO')")
+//@Controller
+//@RequestMapping
+//public class AutorControlador {
+//
+//    @Autowired
+//    private AutorServicio clienteServicios;
+//
+//    @GetMapping("/editar-perfilmascota")
+//    public String editarPerfil(HttpSession session, @RequestParam(required=false) Integer id, ModelMap model) {
+//
+//        
+//        return "autor.html";
+//    }
+//
+//    /**
+//     *
+//     * @param archivo
+//     * @param modelo
+//     * @param session
+//     * @param id
+//     * @param nombre
+//     * @param apellido
+//     * @param email
+//     * @param documento
+//     * @param clave
+//     * @param clave2
+//     * @param idZona
+//     * @return
+//     */
+//    @PreAuthorize("hasAnyRole('ROLE_USUARIO_REGISTRADO')")
+//    @PostMapping("/actualizar-perfilmascota")
+//    public String registrar(ModelMap modelo, HttpSession session, @RequestParam Integer id, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String email, @RequestParam String documento, MultipartFile archivo, @RequestParam String clave, @RequestParam String clave2, @RequestParam Integer idZona) {
+//        Cliente cliente = null;
+//        Cliente login = (Cliente) session.getAttribute("usuariosession");
+//        if (login == null || !login.getId().equals(id)) {
+//            return "redirect:/inicio";
+//        }
+//        try {
+//            cliente = clienteServicios.buscarXId(id);
+//            clienteServicios.modificarCliente(id, documento, nombre, apellido, email, archivo, clave, clave2, idZona);
+//            session.setAttribute("usuariosession", cliente);
+//            return "redirect:/inicio";
+//        } catch (ErrorServicio ex) {
+//            List<Zona> zonas = zonaRepositorio.findAll();
+//            modelo.put("zonas", zonas);
+//            modelo.put("error", ex.getMessage());
+//            modelo.put("perfil", cliente);
+//            return "perfil.html";
+//        }
+//    }
+//}
